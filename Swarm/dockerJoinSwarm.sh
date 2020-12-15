604f72f4b7f2df3551b2b2f0cc1257befcb899e4
Chris Grabosky <chris@grabosky.net>
2020-12-15 10:50:15 -0500

    scale up

--- Swarm/dockerJoinSwarm.sh
+++ Swarm/dockerJoinSwarm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker swarm join --token SWMTKN-1-5fx57mhvrhwcrxnw8s6x78l2qjrfbh9aetn3rn4jw15ea5wude-dsxrdupls2rloehb956fnzzm7 172.31.10.55:2377
+sudo docker swarm join --token SWMTKN-1-3kunnetfpvt2iwhy6mkatq8aojmkyv18yvndjlii31yfm86wz2-945hzvzswqyw8tfo7jv3judwd 172.31.18.115:2377
