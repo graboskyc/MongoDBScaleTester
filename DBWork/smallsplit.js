67a5b5e6dfb9fad391e0c60470f71644010287cf
Chris Grabosky <chris@grabosky.net>
2020-12-14 22:08:23 -0500

    i hate math

--- DBWork/smallsplit.js
+++ DBWork/smallsplit.js
@@ -7,7 +7,6 @@
 var numShards = 8;                            // CHANGE ME: how many shards
 var dbName = "loadtest";                      // CHANGE ME: what DB we using
 var colNames = ["loadtest.queue"];            // CHANGE ME: array of collections we are working with
-var shardPrefix = "atlas-14dpeu-shard-";      // CHANGE ME: from atlas portal
 
 var avgDocSizeKb = 11;                        // CHANGE ME: based on your load generator in locustfile.py
 var keySpaceSize = 16;                        // CHANGE ME: get this from the key space in worker-variables.env but this will break if not 16
@@ -18,24 +17,21 @@ var keyCount = 7;                             // CHANGE ME: get this from the ke
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var expectedDocCount = Math.pow(keySpaceSize, keyCount);
 var CHUNKSIZEKB = 64*1024;                                                          // don't change this
-var numOfChunks = ((expectedDocCount * avgDocSizeKb) / CHUNKSIZEKB) * 2;            // we are doubling because we want the chunks to be half empty
-var chunkSplitInterval = Math.floor(expectedDocCount / numOfChunks);
-var lbound = 0;
-var zoneCardinality = Math.round(expectedDocCount / numShards);
+var chunkSplitInterval = Math.floor(expectedDocCount / numShards);
 var pad = "";
 for (p=0; p < keyCount; p++) {
     pad = pad + "0";
 }
 
-
-
 var db = db.getSiblingDB(dbName);
+sh.stopBalancer();
 
 db.dropDatabase();
 sh.enableSharding(dbName);
 
 print("shard each collection...");
 colNames.forEach(collection => {
+    print("enabling sharding for collection" + collection)
 	sh.shardCollection(collection, {_id : 1}, false);
 });
 
@@ -43,15 +39,19 @@ print("starting presplit...");
 colNames.forEach(collection => {
     print("starting collection: ", collection);
     var ct =0;
-    
-    for (x = lbound + Math.floor(chunkSplitInterval / 2); x < expectedDocCount; x = x + chunkSplitInterval) {
-        var splitId = (pad + x.toString(16)).slice(-pad.length);
-        if(ct%100 == 0) {
-            print("Done at least " + ct + " of " + numOfChunks);
+
+    for(x = 1; x < numShards; x++) {
+        var splitPoint_dec = x * chunkSplitInterval;
+        var splitPoint_hex = (pad + (splitPoint_dec-1).toString(16)).slice(-pad.length);
+        var insidePoint_hex = (pad + (splitPoint_dec-10).toString(16)).slice(-pad.length);
+        print(splitPoint_dec + " " + splitPoint_hex + " " + insidePoint_hex);
+        if(x == 1) {
+
         }
-        db.adminCommand({split : collection, middle : {_id : splitId}});
-        ct++;
-   }
+        db.adminCommand({split : collection, middle : {_id : splitPoint_hex}});
+        sh.moveChunk(dbName + "." + collection, { "_id" : insidePoint_hex }, "atlas-14dpeu-shard-"+(x-1))
+    }
 });
+sh.stopBalancer();
 
 // double check sh.status(); and db.getSiblingDB("loadtest").queue.getShardDistribution();
